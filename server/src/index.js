224bb4f6e6c1a4bc8a03c25c7d010a21bbdca787
Fernando P.S.D <it21085444@my.sliit.lk>
2023-07-26 03:31:25 +0530

    server file added

---
+++ server/src/index.js
@@ -0,0 +1,19 @@
+import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+
+import { userRouter } from "./routes/users.js";
+import { recipesRouter } from "./routes/recipes.js";
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.use("/auth", UserRouter);
+
+mongoose.connect(
+  "mongodb+srv://recipe:recipe@cluster0.d1gsldk.mongodb.net/recipes?retryWrites=true&w=majority"
+);
+
+app.listen(3001, () => console.log("SERVER STARTED"));
---
+++ server/src/routes/recipes.js
@@ -0,0 +1,8 @@
+import express from "express";
+import mongoose from "mongoose";
+import { RecipeModel } from "../models/Recipes";
+
+const Router = express.Router();
+
+
+export {router as recipeRoutes}
